ab037af9d96cd338ab95c2772ace8e77ded845ab
Vikas <vikasmaur10@gmail.com>
2024-01-20 16:12:07 +0530

    added defaultLayoutPlugin to pdf viewer so as to enhance the pdf viewing experience and make it much more accessible

--- src/components/Embed.tsx
+++ src/components/Embed.tsx
@@ -1,17 +1,29 @@
 'use client'
 import React from 'react';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
+import { Worker, Viewer, ProgressBar } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import { pdfjs } from 'react-pdf';
+import { defaultLayoutPlugin} from '@react-pdf-viewer/default-layout';
+
+// Import styles
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
 
 const Embed: React.FC = () => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
     return (
-        <div className="mx-auto mt-16 flex flex-col gap-5 justify-center items-center">
-            <h2 className='w-full px-8 text-2xl font-bold'>Sample Report</h2>
-            <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}>
-                <Viewer fileUrl="https://smriti.co/ielts-writing-checker/assets/assets/YOUR_IELTS_WRITING_BAND.pdf" />
-            </Worker>
-        </div>
+        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}>
+            <div className="mx-auto mt-16 flex flex-col gap-5 justify-center items-center">
+                <h2 className='w-full px-8 text-2xl font-bold'>Sample Report</h2>
+                <div className='w-[95%] h-[200vh]'>
+                    <Viewer plugins={[defaultLayoutPluginInstance]} renderLoader={(percentages: number) => (
+                        <div className='w-full'>
+                            <ProgressBar progress={Math.round(percentages)} />
+                        </div>
+                    )} fileUrl="https://smriti.co/ielts-writing-checker/assets/assets/YOUR_IELTS_WRITING_BAND.pdf" />
+                </div>
+            </div>
+        </Worker>
     )
 }
 
